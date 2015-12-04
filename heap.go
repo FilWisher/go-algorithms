fe4dc79baa0835766c5a0d81010fcafb9bd3a69f
FilWisher <williamfisher@hotmail.co.uk>
2015-12-04 15:42:02 +0000

    adds imperfect insert to heap.go

--- heap.go
+++ heap.go
@@ -4,16 +4,16 @@ import (
   "fmt"
 )
 
-type Tree struct {
-  left *Tree
+type Heap struct {
+  left *Heap
   value int
-  right *Tree
+  right *Heap
 }
 
 /*
 
   INVARIANTS: 
-    - Greatest value stays at top of tree.
+    - Greatest value stays at top of heap.
  
   OPERATIONS
     - Insert
@@ -22,8 +22,38 @@ type Tree struct {
 
 */
 
+/* TODO: shouldn't only build up on the left hand side */
+func insert(heap *Heap, val int) *Heap {
+  if heap == nil {
+    heap = &Heap{ nil, val, nil }
+  } else if val > heap.value {
+    heap.left = insert(heap.left, val)
+  } else if val < heap.value {
+    heap.left = insert(heap.left, heap.value)
+    heap.value = val
+  }
+  return heap
+}
+
+
+func print_heap(heap *Heap) {
+  if heap == nil {
+    return
+  }
+  print_heap(heap.left)
+  fmt.Printf("%d \n", heap.value)
+  print_heap(heap.right)
+}
+
 func main() {
 
-  fmt.Printf("Hello\n")
+  var heap *Heap
+
+  /* TODO: get values from somewhere more interesting (array?) */
+  heap = insert(heap, 1);
+  heap = insert(heap, 3);
+  heap = insert(heap, 2);
+  print_heap(heap);
+
 
 }
